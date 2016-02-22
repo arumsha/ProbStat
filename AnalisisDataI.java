f7dcc6fe223561f194c1408a4baed72e1a5a4a2d
Yuita Arum Sari <y.arumsari@gmail.com>
2016-02-23 00:32:38 +0700

    Added files via upload

---
+++ AnalisisDataI.java
@@ -0,0 +1,96 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package analisisData1;
+
+import analisisdatalanjut.Statistics;
+import java.text.DecimalFormat;
+
+/**
+ *
+ * @author yuitaarumsari
+ */
+public class AnalisisDataI {
+
+    Statistics st = new Statistics();
+    double[] ba, bb;
+    double[] fi;
+    double[] xi;
+    int k_;
+
+    // 1.mengubah data tunggal menjadi data berkelompok
+    public void dataTunggalToKelompok(double[] input) {
+        // mencari jangkauan
+        double j = st.findMax(input) - st.findMin(input);
+        System.out.println("Nilai minimum = " + st.findMin(input));
+        System.out.println("Nilai maximum = " + st.findMax(input));
+        System.out.println("Range = " + j);
+        // mencari banyak kelas dg kaidah empiris Sturgess
+        double log_ = 3.3 * (Math.log(input.length) / Math.log(10));
+        double k = 1 + log_;
+        k_ = (int) Math.round(k);
+        System.out.println("Banyak kelas = " + k_);
+        // mencari panjang kelas dengan rumus
+        int c = (int) Math.round(j / k_);
+        System.out.println("Panjang kelas = " + c);
+        // menentukan frekuensi tiap kelas
+        // batas atas 
+        bb = new double[k_];
+        bb[0] = st.findMin(input);
+        for (int i = 1; i < k_; i++) {
+            bb[i] = bb[i - 1] + c;
+        }
+        // batas bawah 
+        ba = new double[c];
+        for (int i = 0; i < bb.length; i++) {
+            ba[i] = bb[i] + c - 1;
+        }
+        System.out.println("-----Data berkelompok----");
+        for (int i = 0; i < bb.length; i++) {
+            System.out.println(bb[i] + " - " + ba[i]);
+        }
+    }
+
+    // 1b. Mean data tunggal dan mean data kelompok
+    // mean data kelompok
+    public double[] getNilaiTengah() {
+        xi = new double[k_];
+        for (int i = 0; i < xi.length; i++) {
+            xi[i] = (ba[i] + bb[i]) / 2;
+        }
+        return xi;
+    }
+
+    // menghitung frekuensi relatif dari masing-masing kelas kelompok
+    public double[] getFrekuensiRelatif(double[] input) {
+        fi = new double[k_];
+        for (int j = 0; j < fi.length; j++) {
+            int count = 0;
+            for (int i = 0; i < input.length; i++) {
+                if (input[i] >= bb[j] && input[i] <= ba[j]) {
+                    count++;
+                }
+                fi[j] = count;
+            }
+        }
+        return fi;
+    }
+
+    // menghitung mean tiap kelompok
+    public double getMeanDataKelompok() {
+        double meanKelompok = 0;
+        double sigma_fi_xi =0;
+        double sigma_fi = 0;
+        for (int i = 0; i < fi.length; i++) {
+            double a = (fi[i]*getNilaiTengah()[i]);
+            sigma_fi_xi +=a;
+            sigma_fi_xi++;
+            sigma_fi +=fi[i];
+            sigma_fi++;
+        }
+
+        return meanKelompok = Double.parseDouble(
+                new DecimalFormat("##.##").format(sigma_fi_xi/sigma_fi));
+    }
+}
---
+++ MainAnalisisDataI.java
@@ -0,0 +1,65 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package analisisData1;
+
+import analisisdatalanjut.Statistics;
+
+/**
+ *
+ * @author yuitaarumsari
+ */
+public class MainAnalisisDataI {
+    static double[] input_T = new double[]{65,83,88,81,82,60,75,100,93,81,38,90,92,85,
+        76,88,78,74,70,48,80,63,76,49,84,79,80,70,68,92,61,83,89,81,82,72,83,
+        87,81,82,81,91,56,65,63,74,89,73,90,97,48,90,92,85,76,74,88,61,90,97,
+        75,83,67,60,74,51,71,72,82,70,95,72,85,67,88,80,63,76,49,84};
+    
+    static double[] input_U = new double[]{65,83,88,81,82,60,75,98,93,81,38,90,92,85,
+        76,88,78,74,70,48,80,63,76,49,84,79,80,70,68,92,61,83,88,81,82,72,83,87,81,82,
+        81,91,56,65,63,74,89,73,90,97,48,90,92,85,76,74,88,61,90,97,75,83,67,86,80,51,
+        71,72,82,70,93,72,91,67,88,80,63,76,49,84
+    };
+    
+    static AnalisisDataI ad1 = new AnalisisDataI();
+    static Statistics st = new Statistics();
+    public static void main(String[] args) {
+        ad1.dataTunggalToKelompok(input_U);
+        System.out.println("Mean Data Tunggal = "+st.mean(input_U));
+        double[] fr = ad1.getFrekuensiRelatif(input_T);
+        System.out.println("Data berkelompok dan frekuensinya");
+        for (int i = 0; i < fr.length; i++) {
+            System.out.println(ad1.ba[i]+" - "+ad1.ba[i]+" --> "+fr[i]);
+        }
+        System.out.println("Mean Data Berkelompok = "+ad1.getMeanDataKelompok());
+        
+    }
+}
+
+/**
+ * OUTPUT
+ * Nilai minimum = 38.0
+Nilai maximum = 98.0
+Range = 60.0
+Banyak kelas = 7
+Panjang kelas = 9
+-----Data berkelompok----
+38.0 - 46.0
+47.0 - 55.0
+56.0 - 64.0
+65.0 - 73.0
+74.0 - 82.0
+83.0 - 91.0
+92.0 - 100.0
+Mean Data Tunggal = 77.08
+Data berkelompok dan frekuensinya
+46.0 - 46.0 --> 1.0
+55.0 - 55.0 --> 5.0
+64.0 - 64.0 --> 8.0
+73.0 - 73.0 --> 13.0
+82.0 - 82.0 --> 24.0
+91.0 - 91.0 --> 21.0
+100.0 - 100.0 --> 8.0
+Mean Data Berkelompok = 70.67
+ */
